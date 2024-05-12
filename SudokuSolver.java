27b3d74c829fee754bc147df9a6fe40f667a6dba
James <122970798+jamesdeh@users.noreply.github.com>
2024-05-12 18:11:53 +1200

    Update SudokuSolver.java

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,4 +1,4 @@
-public class SudokuSolver {
+public class Main {
     private static final int GRID_SIZE = 9;
     public static void main(String[] args) {
         int board[][] = {
@@ -14,4 +14,45 @@ public class SudokuSolver {
         };
         printBoard(board);
     }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int LocalBoxRow = row - row % 3;
+        int LocalBoxColumn = column - column % 3;
+        for (int i = LocalBoxRow; i < LocalBoxRow + 3; i++) {
+            for (int j = LocalBoxColumn; j < LocalBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private static boolean isValidPlacement(int board[][], int number, int row, int column) {
+        return !isNumberInRow(board, number, row) && !isNumberInColumn(board, number, column)
+                && !isNumberInBox(board, number, row, column);
+    }
+    private static boolean solveboard(int[][] board) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                }
+            }
+        }
+    }
+
 }
